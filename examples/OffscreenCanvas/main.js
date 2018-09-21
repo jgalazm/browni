9687a1ffe43788bbd69bdbc5c46d4e2c18ce5fa5
Jose Galaz <jdgalaz@uc.cl>
2018-09-21 11:38:33 -0300

    offscreen canvas example without showing to screen

---
+++ examples/OffscreenCanvas/main.js
@@ -0,0 +1,56 @@
+let offscreen = new OffscreenCanvas(100,100);
+let data = {
+    bathymetry: [[1,1,1],[1,1,1],[1,1,1]]
+    ,
+    earthquake: [{
+        L: 5,
+        W: 3,
+        depth: 2,
+        slip: 1.0,
+        strike: 30.0,
+        dip: 70.0,
+        rake: -45.0,
+        U3: 1.0,
+        cn: 0,
+        ce: 0,
+        reference: 'center'
+    }],
+    coordinates: 'cartesian',
+    waveWidth: 100,
+    waveHeight: 100,
+    xmin: -10,
+    xmax: 10,
+    ymin: -10,
+    ymax: 10,
+    canvas: offscreen
+}
+
+let output = {
+    stopTime: 10,
+    displayWidth: 512,
+    displayHeight: 512,
+};
+
+let recordedBlobs = [];
+
+lifeCycle = {
+    dataWasLoaded: (model) => {
+        
+        console.log(offscreen);
+        
+        // offscreen.convertToBlob().then(function(blob) {
+        //     let video = document.getElementById('video');
+        //     var superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+        //     video.src = window.URL.createObjectURL(superBuffer);
+        //     console.log(blob);
+        // });
+
+    },
+
+    modelStepDidFinish: (model, controller) => {
+        console.log(model.discretization.stepNumber);
+        return false;
+    }
+}
+
+let thismodel = new NAMI.app(data, output, lifeCycle);
