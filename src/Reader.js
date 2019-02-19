a33877bb957f5554a3f777161245fbfa7344fab6
Jose Galaz <jdgalaz@uc.cl>
2019-02-19 00:39:46 -0300

    add dummy Reader

---
+++ src/Reader.js
@@ -0,0 +1,5 @@
+const Reader = () =>{
+    console.log('reader');
+}
+
+export default Reader;
---
+++ src/__tests__/Reader.test.js
@@ -0,0 +1,5 @@
+import Reader from '../Reader';
+
+test('load Reader', () => {
+    expect(1).toBeDefined();
+});
