cdae08fe8aa357db22b7180c4cf38724347e2f02
Jose Galaz <jdgalaz@uc.cl>
2019-02-19 01:53:41 -0300

    describe tests to be implemented in Model

---
+++ src/__tests__/Model.test.js
@@ -0,0 +1,10 @@
+
+"It loads a bathymetry array into a texture with a difference of less than 1%"
+"It loads an initial surface array into a texture to the shader with a difference of less than 1%"
+
+"It renders Okada initial condition properly, according to validation cases described in the paper"
+
+"It renders asteroid initial condition properly"
+
+
+
