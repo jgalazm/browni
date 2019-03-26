aa0fc921f536baf252a47313ad40b7a266ab6482
José  Galaz <jdgalazm@gmail.com>
2019-03-26 18:57:34 -0300

    move remaining parsing operations to Reader

--- src/Model.js
+++ src/Model.js
@@ -29,9 +29,7 @@ let Model = function(data, output) {
     maxHeightsProgram,
     displayProgram;
 
-  let wave, maxHeights, pcolorDisplay;
-  let displayOption, pois, colormap;
-
+  let wave, maxHeights;
   let {
     domain,
     bathymetry,
@@ -39,66 +37,14 @@ let Model = function(data, output) {
     initialSurface,
     earthquake,
     asteroid,
-    slab
+    slab,
+    pcolorDisplay,
+    displayOption,
+    pois,
+    colormap
   } = data;
 
-  pcolorDisplay = {
-    width: output.displayWidth,
-    height: output.displayHeight
-  };
 
-  displayOption = output.displayOption ? output.displayOption : "heights";
-
-  pois = output.pois ? output.pois : {};
-
-  const cmax = 0.1;
-  const cmin = -0.1;
-  let defaultColormap = {
-    thresholds: [
-      0.0 * (cmax - cmin) + cmin,
-      0.06666666666666667 * (cmax - cmin) + cmin,
-      0.13333333333333333 * (cmax - cmin) + cmin,
-      0.2 * (cmax - cmin) + cmin,
-      0.26666666666666666 * (cmax - cmin) + cmin,
-      0.3333333333333333 * (cmax - cmin) + cmin,
-      0.4 * (cmax - cmin) + cmin,
-      0.49 * (cmax - cmin) + cmin,
-      0.5 * (cmax - cmin) + cmin,
-      0.51 * (cmax - cmin) + cmin,
-      0.6666666666666666 * (cmax - cmin) + cmin,
-      0.7333333333333333 * (cmax - cmin) + cmin,
-      0.8 * (cmax - cmin) + cmin,
-      0.8666666666666667 * (cmax - cmin) + cmin,
-      0.9333333333333333 * (cmax - cmin) + cmin,
-      1.0 * (cmax - cmin) + cmin
-    ],
-
-    rgba: [
-      [0.001462, 0.000466, 0.013866, 1],
-      [0.046915, 0.030324, 0.150164, 0.8],
-      [0.142378, 0.046242, 0.308553, 0.8],
-      [0.258234, 0.038571, 0.406485, 0.8],
-      [0.366529, 0.071579, 0.431994, 0.8],
-      [0.472328, 0.110547, 0.428334, 0.9],
-      [0.578304, 0.148039, 0.404411, 0.8],
-      [0.682656, 0.189501, 0.360757, 0.4],
-      [0.780517, 0.243327, 0.299523, 0],
-      [0.865006, 0.316822, 0.226055, 0.4],
-      [0.929644, 0.411479, 0.145367, 0.8],
-      [0.970919, 0.522853, 0.058367, 0.9],
-      [0.987622, 0.64532, 0.039886, 0.8],
-      [0.978806, 0.774545, 0.176037, 0.8],
-      [0.950018, 0.903409, 0.380271, 0.8],
-      [0.988362, 0.998364, 0.644924, 1]
-    ]
-  };
-
-  colormap = output.colormap !== undefined ? output.colormap : defaultColormap;
-
-  // flatten the array
-  colormap.rgba = colormap.rgba.reduce((a, b) => {
-    return a.concat(b);
-  });
 
   /* 
         Start WebGL context
@@ -2724,7 +2670,6 @@ let Model = function(data, output) {
       bathymetry.array,
       bathymetry.textureId
     );
-    debugger;
 
     if (initialSurface != undefined) {
       initialSurface.texture = createTextureFromMatrix(
