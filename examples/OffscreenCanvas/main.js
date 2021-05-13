388a70a3c45e0bc34e1b8b10aba574d81fb93dde
Jose Galaz <jdgalaz@uc.cl>
2021-05-13 18:31:54 +0200

    reconfigure scenario

--- examples/OffscreenCanvas/main.js
+++ examples/OffscreenCanvas/main.js
@@ -5,11 +5,18 @@ const workerProgram = () => {
 
     const canvas = e.data.elem;
     console.log(canvas);
-    const scenario = {
+    const ds = 15/60;
+    const extent = {
       xmin: -121,
-      xmax: 0,
+      xmax: 25,
       ymin: -60,
-      ymax: 60,
+      ymax: 85,
+    };
+    
+    const scenario = {
+      ...extent,
+      waveWidth: parseInt((extent.xmax-extent.xmin)/ds),
+      waveHeight: parseInt((extent.ymax-extent.ymin)/ds),
       earthquake: {
         ce: -12, //centroid N coordinate, e
         cn: -35,
