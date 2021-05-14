77769043273b1477e06e25941d4e2d87f25668f1
Jose Galaz <jdgalaz@uc.cl>
2021-05-14 13:42:11 +0200

    show fps of main thread on offscreencanvas case

--- examples/OffscreenCanvas/offscreen-canvas/main.js
+++ examples/OffscreenCanvas/offscreen-canvas/main.js
@@ -1,10 +1,11 @@
 
 const workerProgram = () => {
   importScripts("http://localhost:4000/build/nami.js");
-  onmessage = (e) => {
-
-    const canvas = e.data.elem;
-    console.log(canvas);
+  const simulationParameters = {
+    skip: 1
+  }
+  
+  const start = (canvas) => {
     const ds = 15/60;
     const extent = {
       xmin: -121,
@@ -25,25 +26,68 @@ const workerProgram = () => {
     };
 
 
-    const nami = new Nami(scenario, {canvas});
+    const nami = new Nami(scenario, { canvas }, {
+      modelStepDidFinish: (model, thisController) => {
+        if (model.discretization.stepNumber % simulationParameters.skip === 0) {
+          return false;
+        }
+        return true;
+      }
+    });
+  }
+
+  onmessage = (e) => {
+    switch(e.data.msg){
+      case "start":
+        start(e.data.elem);
+        break;
+      case "skip-toggle":
+        simulationParameters.skip = simulationParameters.skip == 1 ? 50 : 1;
+        postMessage(simulationParameters);
+      default:
+    }
+
+
 
   }
 }
-
 
 const createWorker = fn => {
   const URL = window.URL || window.webkitURL;
   return new Worker(URL.createObjectURL(new Blob(["(" + fn + ")()"])));
 };
 
+const worker = createWorker(workerProgram);
+
+let timeEl;
+let t1 = Date.now();
+let t2;
+function tickTimer() {
+  t2 = Date.now();
+  diff = t2 - t1;
+  t1 = t2;
+  timeEl.innerText = `approx ${(1000/diff).toFixed(1)} fps`;
+  window.timerRafId = window.requestAnimationFrame(tickTimer);
+}
+
 const mainProgram = () => {
   const canvas = document.querySelector("#simulation-canvas");
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 
-  worker = createWorker(workerProgram);
   const offscreen = canvas.transferControlToOffscreen();
   worker.postMessage({ msg: "start", elem: offscreen }, [offscreen]);
+
+  timeEl = document.getElementById('timer');
+
+  requestAnimationFrame(tickTimer);
+  
 }
 
+const cycleSpeed = () => {
+  worker.postMessage({msg: "skip-toggle"})
+}
 
+worker.addEventListener("message", (message) => {
+  document.querySelector("#speed-counter").innerText = `Skipping: ${message.data.skip-1} frames`;
+})
