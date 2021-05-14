48db84bf4f9ac1a4314c1fde4b571b3aaacf3c46
Jose Galaz <jdgalaz@uc.cl>
2021-05-14 11:38:28 +0200

    add skip button

--- examples/OffscreenCanvas/regular-canvas/main.js
+++ examples/OffscreenCanvas/regular-canvas/main.js
@@ -1,26 +1,41 @@
+const simulationParameters = {
+  skip: 1
+}
 
 const workerProgram = (canvas) => {
-    const ds = 15/60;
-    const extent = {
-      xmin: -121,
-      xmax: 25,
-      ymin: -60,
-      ymax: 85,
-    };
-    
-    const scenario = {
-      ...extent,
-      waveWidth: parseInt((extent.xmax-extent.xmin)/ds),
-      waveHeight: parseInt((extent.ymax-extent.ymin)/ds),
-      earthquake: {
-        ce: -12, //centroid N coordinate, e
-        cn: -35,
-        Mw: 9.0
-      },
-    };
+  const ds = 15 / 60;
+  const extent = {
+    xmin: -121,
+    xmax: 25,
+    ymin: -60,
+    ymax: 85,
+  };
 
-    const nami = new Nami(scenario, {canvas});
+  const scenario = {
+    ...extent,
+    waveWidth: parseInt((extent.xmax - extent.xmin) / ds),
+    waveHeight: parseInt((extent.ymax - extent.ymin) / ds),
+    earthquake: {
+      ce: -12, //centroid N coordinate, e
+      cn: -35,
+      Mw: 9.0
+    },
+  };
 
+  const nami = new Nami(scenario, { canvas }, {
+    modelStepDidFinish: (model, thisController) => {
+      if (model.discretization.stepNumber % simulationParameters.skip === 0) {
+        return false;
+      }
+      return true;
+    }
+  });
+
+}
+
+const cycleSpeed = () => {
+  simulationParameters.skip = (simulationParameters.skip + 10) % 110;
+  document.querySelector("#speed-counter").innerText = `Skipping: ${simulationParameters.skip-1} frames`;
 }
 
 const mainProgram = () => {
